173cb6048321ad734cc2afcf43d0a57d18bca074
장은지 <smeil123@naver.com>
2021-07-08 21:59:10 +0900

    로깅기능 설정

--- src/main/java/com/springboot/project/web/IndexController.java
+++ src/main/java/com/springboot/project/web/IndexController.java
@@ -3,6 +3,8 @@ package com.springboot.project.web;
 import com.springboot.project.service.PostsService;
 import com.springboot.project.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
@@ -14,10 +16,18 @@ public class IndexController {
     //페이지에 관련된 컨트롤러는 모두 여기에
     private final PostsService postsService;
 
+    //로깅
+     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/")
     public String index(Model model){
         //머스테치 스타터를 compile했기 때문에 자동으로 /src/main/resources/index.mustache 가 붙어서 찾아간다고 보면됨
         model.addAttribute("posts",postsService.findAllDesc());
+        logger.trace("Trace");
+        logger.debug("Debug");
+        logger.info("Info");
+        logger.warn("Warn");
+        logger.error("Error");
         return "index";
     }
 
